a4d51bfe241d604b5e6ecc3dfa412823137d3d33
mdc <201205975@post.au.dk>
2017-09-05 16:02:24 +0200

    Added addworkoutprogram functionality to routes etc

--- app_server/routes/index.ts
+++ app_server/routes/index.ts
@@ -7,5 +7,6 @@ import {WorkoutProgramController} from '../controllers/workoutprogramcontroller'
 /* GET homepage. */
 router.get('/', HomeController.index);
 router.post('/exercise/create', WorkoutProgramController.addExercise)
+router.post('/workoutprograms/create', WorkoutProgramController.PostWorkoutprogram);
  
 export = router;
